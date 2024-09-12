e0ccdc8bb3e9ee3b54894ed979ee2ab1cee71d28
k08e24bryant <syarifsanad1@gmail.com>
2024-09-12 14:48:44 +0700

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+
+<x-layout>
+  
+<x-slot:title> {{ $title }} </x-slot:title>
+  <h3 class="text-xl">Welcome To My About</h3>
+
+  <p> Nama : {{$nama}} </p>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+
+Route::get('/about', function () {
+    return view('about ', ['nama' => 'Syarif Sanad'], ['title' => 'about']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'title'] );
+});
+
